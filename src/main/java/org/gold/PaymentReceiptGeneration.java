2bbcba4da4aaaf2d8782a3a1a7e090ffe825e0f0
Zeeshan Shaikh <zeeshanshaikhonline@gmail.com>
2024-10-31 20:39:23 +0400

    [31102024]: Optimize imports

--- src/main/java/org/gold/PaymentReceiptGeneration.java
+++ src/main/java/org/gold/PaymentReceiptGeneration.java
@@ -16,7 +16,6 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
